b919aa6af04a752659f29537b36d8fec0acfc919
Abhinav Jha <abhi28.jha@gmail.com>
2016-12-26 22:20:30 +0530

    Optimized to O(n*sqrt(n)) from O(n^2)

--- Sieve of Eratosthenes.cpp
+++ Sieve of Eratosthenes.cpp
@@ -15,30 +15,21 @@
 #define set_bit(n, x, i) (n ^ ((-x ^ n) & (1 << i)))
 #define get_bit(n, i) ((n >> i) & 1) 
 using namespace std;
- 
+
 template<typename T>void scan(T&x){bool n=false;register T c=getchar();x=0;if(c=='-'){n=true;c=getchar();}for(;(c>47&&c<58);c=getchar()){x=(x<<1)+(x<<3)+c-48;}if(n){x*=-1;}}
 template<typename T>void allocate(T **x,T n){*x=(T*)malloc(sizeof(T)*n);}
 
-std::vector<int> primes;
 bool sieve[10010];
 
 void pre() {
 	memset(sieve, true, sizeof(sieve));
-	for (int i = 2; i <= 10000; i++) {
+	sieve[0] = sieve[1] = false;
+	for (int i = 2; i <= 100; i++) {
 		if (sieve[i]) {
-			primes.pb(i);
 			for (int j = i+i; j <= 10000; j += i)
 				sieve[j] = false;
 		}
 	}
-}
-
-void print_primes(int n) {
-	for (int i = 0; i < primes.size(); i++) {
-		if (primes[i] > n) break;
-		printf("%d ", primes[i]);
-	}
-	printf("\n");
 }
 
 int main() {
@@ -47,7 +38,10 @@ int main() {
 	scanf("%d", &t);
 	while (t--) {
 		scanf("%d", &n);
-		print_primes(n);
+		for (int i = 1; i <= n; i++)
+			if (sieve[i])
+				printf("%d ", i);
+		printf("\n");
 	}
 	return 0;
 }
